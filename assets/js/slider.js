5786f383fcb6f4f01932d5f7f0fc7161208389f9
Danil-timoshenko <123155866+Chtoooo@users.noreply.github.com>
2023-05-05 15:52:17 +0300

    Добавил адаптив и подправил сайты

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,23 +1,13 @@
-document.addEventListener( 'DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     new Splide('#image-slider', {
-        perPage    : 5,
+        perPage: 5,
         play: true,
-        breakpoints: {
-            640: {
-                perPage: 1,
-            },
-        },
-    } ).mount();
-} );
+    }).mount();
+});
 
-document.addEventListener( 'DOMContentLoaded', function () {
-  new Splide('#image-slider-man', {
-      perPage    : 5,
-      play: true,
-      breakpoints: {
-          640: {
-              perPage: 1,
-          },
-      },
-  } ).mount();
-} );
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-slider-man', {
+        perPage: 5,
+        play: true,
+    }).mount();
+});
